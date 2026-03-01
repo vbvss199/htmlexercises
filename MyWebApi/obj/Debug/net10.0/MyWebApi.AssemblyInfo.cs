0f228989bdd15a3e2cba010ff26d164d9d1a13b7
vbvss199 <vbvss1999@gmail.com>
2026-02-28 19:46:38 -0500

    done for the day and closing

--- MyWebApi/obj/Debug/net10.0/MyWebApi.AssemblyInfo.cs
+++ MyWebApi/obj/Debug/net10.0/MyWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e9171000b4e0ec11bb23ebd2f07747dd5ee04a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10bffb6c95a0d14d00853973df7b8df1e3d95f56")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
